3dd3d34bef95598368469fc35317dfacca40b3c6
Kelton Zhang <zhuoran2@andrew.cmu.edu>
2021-07-01 18:34:59 +0800

    feat: segmenting

--- tests/unit/test_exec.py
+++ tests/unit/test_exec.py
@@ -1,9 +1,84 @@
 __copyright__ = "Copyright (c) 2020-2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from jina.executors import BaseExecutor
+import numpy as np
 
+from jina.executors import BaseExecutor
+from jinahub.segmenter.vggish_audio_segmenter import VGGishSegmenter
 
 def test_exec():
     ex = BaseExecutor.load_config('../../config.yml')
     assert ex._dim == 128
+
+
+
+
+def test_sliding_window_mono():
+    n_frames = 100
+    frame_length = 2048
+    signal_orig = np.random.randn(frame_length * n_frames)
+
+    segmenter = VGGishSegmenter(frame_length, frame_length // 2)
+    segmented_chunks_per_doc = segmenter.segment(np.stack([signal_orig, signal_orig]))
+    assert len(segmented_chunks_per_doc) == 2
+    for segmented_chunk in segmented_chunks_per_doc:
+        assert len(segmented_chunk) == n_frames * 2 - 1
+
+
+def test_sliding_window_stereo():
+    n_frames = 100
+    frame_length = 2048
+    signal_orig = np.random.randn(2, frame_length * n_frames)
+
+    segmenter = VGGishSegmenter(frame_length, frame_length // 2)
+    segmented_chunks_per_doc = segmenter.segment(np.stack([signal_orig, signal_orig]))
+    assert len(segmented_chunks_per_doc) == 2
+    for segmented_chunk in segmented_chunks_per_doc:
+        assert len(segmented_chunk) == (n_frames * 2 - 1) * 2
+
+
+def test_location_mono():
+    frame_length = 10
+    frame_overlap_length = frame_length // 2
+    hop_length = frame_length - frame_overlap_length
+    n_frames = 5
+    num_docs = 3
+
+    signal_orig = np.random.randn(frame_length * n_frames)
+    expected_n_frames = (signal_orig.shape[0] - frame_length) / hop_length + 1
+    expected_locations = [[i * hop_length, i * hop_length + frame_length] for i in range(int(expected_n_frames))]
+    expected_channel = 'mono'
+
+    segmenter = VGGishSegmenter(frame_length=frame_length, frame_overlap_length=frame_overlap_length)
+    docs = segmenter.segment(np.stack([signal_orig] * num_docs))
+
+    assert len(docs) == num_docs
+    for d in docs:
+        assert len(d) == expected_n_frames
+        for i, chunk in enumerate(d):
+            assert chunk['location'] == expected_locations[int(i % expected_n_frames)]
+            assert chunk['tags']['channel'] == expected_channel
+
+
+def test_location_stereo():
+    frame_length = 10
+    frame_overlap_length = frame_length // 2
+    hop_length = frame_length - frame_overlap_length
+    n_frames = 5
+    num_docs = 3
+    num_channels = 2
+
+    signal_orig = np.random.randn(num_channels, frame_length * n_frames)
+    expected_n_frames = (signal_orig.shape[1] - frame_length) / hop_length + 1
+    expected_locations = [[i * hop_length, i * hop_length + frame_length] for i in range(int(expected_n_frames))]
+
+    segmenter = VGGishSegmenter(frame_length=frame_length, frame_overlap_length=frame_overlap_length)
+    docs = segmenter.segment(np.stack([signal_orig] * num_docs))
+
+    assert len(docs) == num_docs
+    for d in docs:
+        assert len(d) == expected_n_frames * num_channels
+        for i, chunk in enumerate(d):
+            assert chunk['location'] == expected_locations[int(i % expected_n_frames)]
+            expected_channel = 'left' if i // expected_n_frames == 0 else 'right'
+            assert chunk['tags']['channel'] == expected_channel
--- vggish_audio_segmenter.py
+++ vggish_audio_segmenter.py
@@ -9,7 +9,7 @@ from jina import Executor, DocumentArray, requests
 
 class VGGishSegmenter(Executor):
     """
-    Encode the Document blob into embedding.
+    Segment the Document blob into audio regions.
 
     :param embedding_dim: the output dimensionality of the embedding
     """
@@ -18,14 +18,43 @@ class VGGishSegmenter(Executor):
         super().__init__(*args, **kwargs)
         self._dim = embedding_dim
 
+        def _segment(self):
+            pass # to do
+
     @requests
     def segment(self, docs: Optional[DocumentArray], **kwargs):
         """
-        Encode all docs with images and store the encodings in the embedding attribute of the docs.
-        :param docs: documents sent to the encoder. The docs must have `blob` of the shape `256`.
+        Encode all docs with audio and store the segmented regions in the attribute of the docs.
+        :param docs: documents sent to the segmenter. The docs must have `blob` of the shape `256`.
         """
         if not docs:
             return
+        docs = self._get_input_data(docs)
         for doc in docs:
             assert doc.blob.shape[0] == 256
-            doc.embedding = np.random.rand(self._dim)
+            channel_frames = self._segment(doc.blob)
+
+            chunks = []
+
+            channel_tags = ('mono',) if len(channel_frames) == 1 else ('left', 'right')
+
+            for frames, tag in zip(channel_frames, channel_tags):
+                start = 0
+                for idx, frame in enumerate(frames):
+                    chunks.append(dict(offset=idx, weight=1.0, blob=frame, location=[start, start + len(frame)],
+                                       tags={'channel': tag}))
+                    start += self.hop_length
+            doc.chunks = chunks
+
+        def _get_input_data(self, docs: DocumentArray, parameters: dict):
+            """Create a filtered set of Documents to iterate over."""
+
+            traversal_paths = parameters.get('traversal_paths', self.default_traversal_paths)
+
+            # traverse thought all documents which have to be processed
+            flat_docs = docs.traverse_flat(traversal_paths)
+
+            # filter out documents without audio
+            filtered_docs = DocumentArray([doc for doc in flat_docs if doc.blob is not None])
+
+            return filtered_docs
